e8617c217e6c00b9cdf82dabadd7f92150858dea
Nurlan <nurlanasj@gmail.com>
2022-07-06 13:43:28 +0300

    Commit 2

---
+++ src/test/java/com/example/demo/DataControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import com.example.demo.model.DataController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class DataControllerTest {
+
+        @Autowired
+        DataController dataController;
+
+        @Test
+        void health() {
+            assertEquals( "Health check ok", dataController.healthCheck());
+        }
+
+        @Test
+        void version() {
+            assertEquals( "The actual version is 1.0.0", dataController.version());
+        }
+
+        @Test
+        void nationsLength() {
+            Integer nationsLength = dataController.getRandomNations().size();
+            assertEquals(10, nationsLength);
+        }
+
+        @Test
+        void currenciesLength() {
+            Integer currenciesLength = dataController.getRandomCurrencies().size();
+            assertEquals(20, currenciesLength);
+        }
+}
